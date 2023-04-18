c6c5fa2efaa94df26722bef40c48e5f61a925893
Flavio Assis <flavioh.dev@gmail.com>
2023-04-18 11:41:00 -0300

    Ajustes

--- src/training/pages/ListTrainings.page.tsx
+++ src/training/pages/ListTrainings.page.tsx
@@ -12,25 +12,29 @@ export const ListTrainings = () => {
 		const trainings = data || [];
 
 		return (
-			<main className='flex flex-col min-h-screen items-center p-6 gap-2'>
-				{trainings.length ? (
-					trainings.map(t => (
-						<CardTraining
-							key={t.id}
-							id={t.id}
-							workoutSheetId={t.workoutSheet.id}
-							exerciseId={t.exercise.id}
-							exerciseName={t.exercise.name}
-							sets={t.sets}
-							reps={t.reps}
-							restTime={t.restTime}
-							obs={t.obs}
-							position={t.position}
-						/>
-					))
-				) : (
-					<p>Sem treinos</p>
-				)}
+			<main className='flex flex-col min-h-screen items-center p-6 gap-6'>
+				<h1 className='text-2xl font-bold'>Meus treinos</h1>
+
+				<div className='flex flex-col gap-3 w-full'>
+					{trainings.length ? (
+						trainings.map(t => (
+							<CardTraining
+								key={t.id}
+								id={t.id}
+								workoutSheetId={t.workoutSheet.id}
+								exerciseId={t.exercise.id}
+								exerciseName={t.exercise.name}
+								sets={t.sets}
+								reps={t.reps}
+								restTime={t.restTime}
+								obs={t.obs}
+								position={t.position}
+							/>
+						))
+					) : (
+						<p>Sem treinos</p>
+					)}
+				</div>
 			</main>
 		);
 	}
