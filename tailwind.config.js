49ced37a04d16ca07090ab8c0db87749a4b46e5f
Flavio Assis <flavioh.dev@gmail.com>
2023-04-18 11:34:58 -0300

    Add jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	content: [
+		'./src/pages/**/*.{ts}',
+		'./src/**/pages/*.{tsx}',
+		'./src/**/components/**/*.{tsx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+	plugins: [],
+};
