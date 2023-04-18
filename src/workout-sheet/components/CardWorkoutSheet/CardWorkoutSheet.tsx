110b55f3d3e435af1abcc97c1ffb42be2255f161
Flavio Assis <flavioh.dev@gmail.com>
2023-04-18 17:44:30 -0300

    Ajustes

--- src/workout-sheet/components/CardWorkoutSheet/CardWorkoutSheet.tsx
+++ src/workout-sheet/components/CardWorkoutSheet/CardWorkoutSheet.tsx
@@ -1,45 +1,84 @@
+import { useState } from 'react';
 import { HiPencil } from 'react-icons/hi';
 import { TbMenuOrder } from 'react-icons/tb';
 import { IoTrash } from 'react-icons/io5';
 import { WorkoutSheet } from '@/workout-sheet/workout-sheet.types';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Modal } from '../Modal/Modal';
 
 type Props = WorkoutSheet;
 
 export const CardWorkoutSheet = ({ id, name, position, trainingCount }: Props) => {
+	const router = useRouter();
+
+	const [open, setOpen] = useState(false);
+
+	const handleListTrainings = () => {
+		router.push({
+			pathname: '/trainings',
+			query: { workoutSheetId: id },
+		});
+	};
+
+	const toggleModal = () => setOpen(prev => !prev);
+
 	return (
-		<Link
-			className='flex justify-between items-center p-6 rounded-md w-full bg-white shadow-md transition duration-300 ease-in-out transform-gpu hover:shadow-xl hover:-translate-y-1'
-			role='button'
-			href={{
-				pathname: '/trainings',
-				query: { workoutSheetId: id },
-			}}
-		>
-			<div className='flex flex-1'>
-				<span className='font-semibold text-xl leading-10'>{name}</span>
-			</div>
+		<>
+			<div className='flex justify-between items-center p-6 rounded-md w-full bg-white shadow-md transition duration-300 ease-in-out transform-gpu hover:shadow-xl hover:-translate-y-1'>
+				<button
+					className='flex flex-1'
+					onClick={handleListTrainings}
+					title='Visualizar os treinos'
+				>
+					<span className='font-semibold text-xl leading-10'>{name}</span>
+				</button>
 
-			<div className='flex flex-1 justify-between'>
-				<span className='text-xl leading-10'>{trainingCount} treinos</span>
-
-				<div className='flex gap-2 items-center'>
-					<a
-						role='button'
-						title='Editar nome da ficha'
+				<div className='flex flex-1 justify-between'>
+					<button
+						className='flex flex-1'
+						onClick={handleListTrainings}
+						title='Visualizar os treinos'
 					>
-						<HiPencil className='text-3xl hover:text-green-500 transition duration-300 ease-in-out' />
-					</a>
+						<span className='text-xl leading-10'>{trainingCount} treinos</span>
+					</button>
 
-					<a role='button'>
-						<TbMenuOrder className='text-3xl hover:text-blue-500 transition duration-300 ease-in-out' />
-					</a>
+					<div className='flex gap-2 items-center'>
+						<Link
+							role='button'
+							href='#'
+							title='Editar nome da ficha'
+						>
+							<HiPencil className='text-3xl hover:text-green-500 transition duration-300 ease-in-out' />
+						</Link>
 
-					<a role='button'>
-						<IoTrash className='text-3xl hover:text-red-500 transition duration-300 ease-in-out' />
-					</a>
+						<Link
+							role='button'
+							href='#'
+							title='Reordenar fichas'
+						>
+							<TbMenuOrder className='text-3xl hover:text-blue-500 transition duration-300 ease-in-out' />
+						</Link>
+
+						<a
+							role='button'
+							href='#'
+							title='Excluir ficha'
+							onClick={toggleModal}
+						>
+							<IoTrash className='text-3xl hover:text-red-500 transition duration-300 ease-in-out' />
+						</a>
+					</div>
 				</div>
 			</div>
-		</Link>
+
+			<Modal
+				id={id}
+				open={open}
+				toggleModal={toggleModal}
+				trainingCount={trainingCount}
+				workoutSheetName={name}
+			/>
+		</>
 	);
 };
