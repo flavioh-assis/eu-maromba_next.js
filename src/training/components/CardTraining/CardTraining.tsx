0c53049416d5d5b7fe89b042a9cc806450de10e3
Flavio Assis <flavioh.dev@gmail.com>
2023-04-19 19:46:19 -0300

    Ajustes estilos mobile

--- src/training/components/CardTraining/CardTraining.tsx
+++ src/training/components/CardTraining/CardTraining.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import gifAbs from './abs (1).gif';
+import gifAbs from './abs.gif';
 
 type TrainingProps = {
 	id: number;
@@ -25,14 +25,14 @@ export const CardTraining = ({
 	sets,
 }: TrainingProps) => {
 	return (
-		<div className='flex flex-col w-full p-4 gap-2 rounded-md shadow-md bg-white md:flex-row md:justify-start md:gap-10'>
+		<div className='flex flex-col w-full gap-2 p-4 bg-white rounded-md shadow-md sm:flex-row sm:justify-start sm:gap-10'>
 			<Image
-				className='object-contain h-full w-full m-auto rounded-md max-h-96 md:w-48 md:max-h-48'
+				className='object-contain w-full h-full m-auto rounded-md max-h-96 sm:w-48 sm:max-h-48'
 				src={gifAbs}
 				alt=''
 			/>
 
-			<div className='flex flex-col flex-1 h-full w-full gap-2'>
+			<div className='flex flex-col flex-1 w-full h-full gap-2'>
 				<h5 className='text-xl font-bold text-gray-800'>{exerciseName}</h5>
 
 				<div className='flex flex-col'>
@@ -46,7 +46,7 @@ export const CardTraining = ({
 						{restTime} segundos de descanso
 					</span>
 
-					<span className='font-normal mt-2 text-gray-700'>
+					<span className='mt-2 font-normal text-gray-700'>
 						Observação: {obs || 'nenhuma'}
 					</span>
 				</div>
