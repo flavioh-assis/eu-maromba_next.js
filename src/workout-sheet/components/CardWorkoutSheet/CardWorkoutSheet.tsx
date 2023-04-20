ddcac7916dc5a7d28cc260beabb795d803daafac
Flavio Assis <flavioh.dev@gmail.com>
2023-04-20 14:59:16 -0300

    Cria Modal de delete

--- src/workout-sheet/components/CardWorkoutSheet/CardWorkoutSheet.tsx
+++ src/workout-sheet/components/CardWorkoutSheet/CardWorkoutSheet.tsx
@@ -5,7 +5,7 @@ import { IoTrash } from 'react-icons/io5';
 import { WorkoutSheet } from '@/workout-sheet/workout-sheet.types';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Modal } from '../Modal/Modal';
+import { ModalDelete } from '../ModalDelete/ModalDelete';
 
 type Props = WorkoutSheet;
 
@@ -15,17 +15,20 @@ export const CardWorkoutSheet = ({ id, name, position, trainingCount }: Props) =
 	const [open, setOpen] = useState(false);
 
 	const handleListTrainings = () => {
-		router.push({
-			pathname: '/trainings',
-			query: { workoutSheetId: id },
-		});
+		router.push(
+			{
+				pathname: '/trainings',
+				query: { workoutSheetId: id },
+			},
+			'/trainings'
+		);
 	};
 
 	const toggleModal = () => setOpen(prev => !prev);
 
 	return (
 		<>
-			<div className='flex items-center justify-between w-full px-4 transition duration-300 bg-white rounded-md shadow-md hover:shadow-xl hover:-translate-y-1 sm:text-sm'>
+			<div className='flex items-center justify-between w-full px-2 transition duration-300 bg-white rounded-md shadow-md md:px-4 hover:shadow-xl hover:-translate-y-1 sm:text-sm'>
 				<button
 					className='flex w-1/2 items-center min-h-[4rem] sm:min-h-[8rem] text-left mr-1 bg-red-0'
 					onClick={handleListTrainings}
@@ -43,13 +46,13 @@ export const CardWorkoutSheet = ({ id, name, position, trainingCount }: Props) =
 						<span className='text-sm sm:text-xl'>{trainingCount} treinos</span>
 					</button>
 
-					<div className='flex items-center gap-2'>
+					<div className='flex items-center gap-1 sm:gap-2'>
 						<Link
 							role='button'
 							href='#'
 							title='Editar nome da ficha'
 						>
-							<HiPencil className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:text-green-500' />
+							<HiPencil className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
 						</Link>
 
 						<Link
@@ -57,7 +60,7 @@ export const CardWorkoutSheet = ({ id, name, position, trainingCount }: Props) =
 							href='#'
 							title='Reordenar fichas'
 						>
-							<TbMenuOrder className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:text-blue-500' />
+							<TbMenuOrder className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
 						</Link>
 
 						<a
@@ -65,18 +68,18 @@ export const CardWorkoutSheet = ({ id, name, position, trainingCount }: Props) =
 							title='Excluir ficha'
 							onClick={toggleModal}
 						>
-							<IoTrash className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:text-red-500' />
+							<IoTrash className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
 						</a>
 					</div>
 				</div>
 			</div>
 
-			<Modal
+			<ModalDelete
 				id={id}
 				open={open}
-				toggleModal={toggleModal}
 				trainingCount={trainingCount}
 				workoutSheetName={name}
+				toggleModal={toggleModal}
 			/>
 		</>
 	);
---
+++ src/workout-sheet/components/ModalDelete/ModalDelete.tsx
@@ -0,0 +1,64 @@
+import { useMutationDeleteWorkoutSheet } from '@/workout-sheet/workout-sheet.service';
+import { Modal } from '../Modal/Modal';
+import { AxiosError } from 'axios';
+
+type Props = {
+	id: number;
+	open: boolean;
+	trainingCount: number;
+	workoutSheetName: string;
+	toggleModal: VoidFunction;
+};
+
+export const ModalDelete = ({
+	id,
+	open,
+	trainingCount,
+	workoutSheetName,
+	toggleModal,
+}: Props) => {
+	const { mutate } = useMutationDeleteWorkoutSheet();
+
+	const handleDelete = () => {
+		mutate(id, {
+			onError: e => {
+				const { response } = e as unknown as AxiosError;
+				const { message } = response?.data as { message: string };
+
+				alert(message);
+			},
+			onSuccess: () => {
+				toggleModal();
+			},
+		});
+	};
+
+	const trainingText = () => {
+		return trainingCount > 0 ? (
+			<>
+				A ficha <strong>possui {trainingCount} treinos. </strong>
+				<span>Todos serão excluídos!</span>
+			</>
+		) : (
+			<span>A ficha não possui treinos.</span>
+		);
+	};
+
+	return (
+		<Modal
+			id={id}
+			open={open}
+			title='Excluir ficha'
+			handleConfirm={handleDelete}
+			toggleModal={toggleModal}
+		>
+			<div className='flex flex-col'>
+				<p>
+					Deseja remover a ficha <strong>{workoutSheetName}</strong>?
+				</p>
+				<p>{trainingText()}</p>
+				<p>Essa ação não poderá ser desfeita.</p>
+			</div>
+		</Modal>
+	);
+};
