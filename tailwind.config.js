62efeb83fb5d0868dd20f287e42f64a407436d4c
Flavio Assis <flavioh.dev@gmail.com>
2023-04-24 17:40:19 -0300

    Add toastify

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
 			backgroundColor: {
 				'black-80': 'rgba(0,0,0,.8)',
 			},
+			boxShadow: {
+				card: 'rgba(0, 0, 0, 0.08) 0px 6px 16px -8px, rgba(0, 0, 0, 0.05) 0px 9px 28px, rgba(0, 0, 0, 0.03) 0px 12px 48px 16px',
+			},
 		},
 	},
 	purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
