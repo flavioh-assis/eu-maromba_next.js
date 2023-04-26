69773355a90d2f4d20fc2026740e218ed5cd4f63
Flavio Assis <flavioh.dev@gmail.com>
2023-04-26 12:07:06 -0300

    Consome modal de criar WS

--- src/workout-sheet/pages/ListWorkoutSheet/ListWorkoutSheet.page.tsx
+++ src/workout-sheet/pages/ListWorkoutSheet/ListWorkoutSheet.page.tsx
@@ -1,9 +1,15 @@
 import { CardWorkoutSheet } from '@/workout-sheet/components';
+import { ModalCreate } from '@/workout-sheet/components';
 import { useGetAllWorkoutSheets } from '@/workout-sheet/workout-sheet.service';
+import { useState } from 'react';
 
 export const ListWorkoutSheet = () => {
 	const { data } = useGetAllWorkoutSheets();
 	const workoutSheets = data || [];
+
+	const [openModal, setOpenModal] = useState(false);
+
+	const toggleModal = () => setOpenModal(open => !open);
 
 	return (
 		<main className='flex flex-col items-center min-h-screen gap-2 p-2 sm:gap-6 sm:p-6'>
@@ -23,7 +29,19 @@ export const ListWorkoutSheet = () => {
 				) : (
 					<p>Você não criou nenhuma ficha :(</p>
 				)}
+
+				<button
+					onClick={toggleModal}
+					className='px-4 py-2 my-3 text-white transition duration-300 bg-blue-600 rounded-md shadow-md hover:bg-blue-500'
+				>
+					Criar ficha
+				</button>
 			</div>
+
+			<ModalCreate
+				open={openModal}
+				toggleModal={toggleModal}
+			/>
 		</main>
 	);
 };
