3612b8e6c547c5b7c8030cb5385df7aef7ef161b
Flavio Assis <flavioh.dev@gmail.com>
2023-04-26 12:04:16 -0300

    Indexa componentes

--- src/workout-sheet/components/index.tsx
+++ src/workout-sheet/components/index.tsx
@@ -1,1 +1,5 @@
 export { CardWorkoutSheet } from './CardWorkoutSheet/CardWorkoutSheet';
+export { Modal } from './Modal/Modal';
+export { ModalCreate } from './ModalCreate/ModalCreate';
+export { ModalUpdate } from './ModalUpdate/ModalUpdate';
+export { ModalDelete } from './ModalDelete/ModalDelete';
