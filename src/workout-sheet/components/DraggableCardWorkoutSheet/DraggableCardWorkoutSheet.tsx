4542aeba997d64061562d88a53e0349d0041cf01
Flavio Assis <flavioh.dev@gmail.com>
2023-05-02 11:18:06 -0300

    Ajustes CSS

--- src/workout-sheet/components/DraggableCardWorkoutSheet/DraggableCardWorkoutSheet.tsx
+++ src/workout-sheet/components/DraggableCardWorkoutSheet/DraggableCardWorkoutSheet.tsx
@@ -14,50 +14,30 @@ export const DraggableCardWorkoutSheet = ({
 	trainingCount,
 	cardIndex,
 }: Props) => {
-	// const getItemStyle = (isDragging: boolean, draggableStyle: any) => {
-	// 	const grid = 28;
-
-	// 	return {
-	// 		// some basic styles to make the items look a bit nicer
-	// 		userSelect: 'none',
-	// 		// padding: grid * 2,
-	// 		// marginBottom: `12px`,
-
-	// 		// change background colour if dragging
-	// 		background: isDragging ? 'lightgreen' : 'white',
-
-	// 		// styles we need to apply on draggables
-	// 		...draggableStyle,
-	// 	};
-	// };
-
 	return (
 		<Draggable
 			key={id}
 			draggableId={String(id)}
 			index={cardIndex}
 		>
-			{(provided, snapshot) => (
+			{(provided, _) => (
 				<div
-					className='flex items-center justify-between w-full px-2 m-3 transition-all duration-500 bg-white rounded-md shadow-sm sm:px-4 hover:shadow-card sm:text-sm'
+					className='flex items-center justify-between w-full px-2 mb-3 transition-shadow duration-500 bg-white rounded-md shadow-sm sm:px-4 hover:shadow-card sm:text-sm'
 					ref={provided.innerRef}
 					{...provided.draggableProps}
 					{...provided.dragHandleProps}
-					// style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
 				>
 					<div className='flex w-1/2 items-center min-h-[4rem] sm:min-h-[8rem] text-left mr-1 bg-red-0'>
 						<span className='text-sm font-semibold sm:text-xl'>{name}</span>
 					</div>
 
-					<div className='flex w-1/2 gap-2'>
-						<div className='flex w-full items-center min-h-[4rem] sm:min-h-[8rem]'>
-							<span className='text-sm sm:text-xl'>
-								{trainingCount} {trainingCount != 1 ? 'treinos' : 'treino'}
-							</span>
-						</div>
+					<div className='flex items-center justify-between w-1/2'>
+						<span className='text-sm sm:text-xl'>
+							{trainingCount} {trainingCount === 1 ? 'treino' : 'treinos'}
+						</span>
 
-						<div className='flex items-center scale-150'>
-							<MdDragIndicator />
+						<div className='flex items-center'>
+							<MdDragIndicator className='text-xl transition duration-300 ease-in-out hover:scale-125 sm:text-3xl' />
 						</div>
 					</div>
 				</div>
