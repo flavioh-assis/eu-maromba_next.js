5ad8df2cb7220be66cae0ee4d9b67a6a24483086
Flavio Assis <flavioh.dev@gmail.com>
2023-05-02 18:56:07 -0300

    Transforma CardWS em draggable

--- src/workout-sheet/components/CardWorkoutSheet/CardWorkoutSheet.tsx
+++ src/workout-sheet/components/CardWorkoutSheet/CardWorkoutSheet.tsx
@@ -1,16 +1,27 @@
 import { useState } from 'react';
 import { HiPencil } from 'react-icons/hi';
-import { TbMenuOrder } from 'react-icons/tb';
 import { IoTrash } from 'react-icons/io5';
-import { WorkoutSheet } from '@/workout-sheet/workout-sheet.types';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { ModalDelete, ModalUpdate } from '@/workout-sheet/components';
-import { Card, CardTitle, TrainingAmount } from '@/styles/styled';
+import { Card, CardTitle, FlexRow, TrainingAmount } from '@/styles/styled';
+import { MdDragIndicator } from 'react-icons/md';
+import { Draggable } from 'react-beautiful-dnd';
 
-type Props = WorkoutSheet;
+type Props = {
+	id: number;
+	name: string;
+	trainingCount: number;
+	cardIndex: number;
+	isDraggable: boolean;
+};
 
-export const CardWorkoutSheet = ({ id, name, position, trainingCount }: Props) => {
+export const CardWorkoutSheet = ({
+	id,
+	name,
+	trainingCount,
+	cardIndex,
+	isDraggable,
+}: Props) => {
 	const router = useRouter();
 
 	const [openModalDelete, setOpenModalDelete] = useState(false);
@@ -32,51 +43,57 @@ export const CardWorkoutSheet = ({ id, name, position, trainingCount }: Props) =
 
 	return (
 		<>
-			<Card>
-				<button
-					className='flex w-1/2 items-center min-h-[4rem] sm:min-h-[8rem] text-left mr-1 bg-red-0'
-					onClick={handleListTrainings}
-					title='Visualizar os treinos'
-				>
-					<CardTitle>{name}</CardTitle>
-				</button>
-
-				<div className='flex w-1/2 gap-2'>
-					<button
-						onClick={handleListTrainings}
-						title='Visualizar os treinos'
-						className='flex w-full items-center min-h-[4rem] sm:min-h-[8rem]'
+			<Draggable
+				key={id}
+				draggableId={String(id)}
+				index={cardIndex}
+				isDragDisabled={false}
+			>
+				{(provided, _) => (
+					<Card
+						isDraggable={true}
+						ref={provided.innerRef}
+						{...provided.draggableProps}
+						{...provided.dragHandleProps}
 					>
-						<TrainingAmount>
-							{trainingCount} {trainingCount != 1 ? 'treinos' : 'treino'}
-						</TrainingAmount>
-					</button>
-
-					<div className='flex items-center gap-1 sm:gap-2'>
 						<button
-							title='Editar nome da ficha'
-							onClick={toggleModalUpdate}
+							title='Visualizar os treinos'
+							onClick={!isDraggable ? handleListTrainings : () => {}}
+							className='flex w-11/12 sm:w-10/12 items-center min-h-[4rem] sm:min-h-[8rem] text-left'
 						>
-							<HiPencil className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
+							<FlexRow className='w-3/5'>
+								<CardTitle>{name}</CardTitle>
+							</FlexRow>
+
+							<TrainingAmount className='w-2/5 '>
+								{trainingCount} {trainingCount != 1 ? 'treinos' : 'treino'}
+							</TrainingAmount>
 						</button>
 
-						<Link
-							role='button'
-							href='/workout-sheets/reorder'
-							title='Reordenar fichas'
-						>
-							<TbMenuOrder className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
-						</Link>
+						<div className='flex items-center justify-end w-1/12 gap-1 sm:w-fit sm:gap-2'>
+							{isDraggable ? (
+								<MdDragIndicator className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
+							) : (
+								<>
+									<button
+										title='Editar nome da ficha'
+										onClick={toggleModalUpdate}
+									>
+										<HiPencil className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
+									</button>
 
-						<button
-							title='Excluir ficha'
-							onClick={toggleModalDelete}
-						>
-							<IoTrash className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
-						</button>
-					</div>
-				</div>
-			</Card>
+									<button
+										title='Excluir ficha'
+										onClick={toggleModalDelete}
+									>
+										<IoTrash className='text-xl transition duration-300 ease-in-out sm:text-3xl hover:scale-125' />
+									</button>
+								</>
+							)}
+						</div>
+					</Card>
+				)}
+			</Draggable>
 
 			<ModalDelete
 				id={id}
