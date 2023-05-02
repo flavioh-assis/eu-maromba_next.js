78fbbc453293f2fcb2a102dcd3f906667f7caac9
Flavio Assis <flavioh.dev@gmail.com>
2023-05-02 11:48:49 -0300

    Ajustes CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,6 @@ module.exports = {
 			backgroundColor: {
 				'black-80': 'rgba(0,0,0,.8)',
 			},
-			boxShadow: {
-				card: '20px 20px 60px #d0d0d2, -20px -20px 60px #ffffff',
-			},
 		},
 	},
 	purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
